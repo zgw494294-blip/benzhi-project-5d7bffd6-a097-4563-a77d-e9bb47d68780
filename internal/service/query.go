42343aa1013d4db1a7772235d2111ebf5db23be6
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 5d7bffd6-a097-4563-a77d-e9bb47d68780-bug-6

--- internal/service/query.go
+++ internal/service/query.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"sync"
 
 	"groundwater-release/internal/audit"
 	"groundwater-release/internal/domain"
@@ -18,6 +19,41 @@ type verificationCall struct {
 	done   chan struct{}
 	report audit.FullChainVerification
 	err    error
+
+	mu         sync.Mutex
+	waiters    int
+	workCancel context.CancelFunc
+}
+
+// register records a new waiter for this shared verification. It returns true
+// when the waiter has been admitted, or false when the work already finished
+// (in which case the caller should read call.report/call.err directly).
+func (c *verificationCall) register() bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	select {
+	case <-c.done:
+		return false
+	default:
+	}
+	c.waiters++
+	return true
+}
+
+// leave decrements the waiter count and, when no waiter remains interested,
+// cancels the shared background context so the verification worker stops
+// holding database resources.
+func (c *verificationCall) leave() {
+	c.mu.Lock()
+	if c.waiters > 0 {
+		c.waiters--
+	}
+	if c.waiters == 0 {
+		if c.workCancel != nil {
+			c.workCancel()
+		}
+	}
+	c.mu.Unlock()
 }
 
 func (s *Service) CampaignDetail(ctx context.Context, campaignID string) (CampaignDetail, error) {
@@ -64,6 +100,10 @@ func (s *Service) CredentialVerification(ctx context.Context, campaignID string)
 	s.verificationMu.Lock()
 	if call, ok := s.verificationCalls[campaignID]; ok {
 		s.verificationMu.Unlock()
+		if !call.register() {
+			return call.report, call.err
+		}
+		defer call.leave()
 		select {
 		case <-ctx.Done():
 			return audit.FullChainVerification{}, ctx.Err()
@@ -71,16 +111,30 @@ func (s *Service) CredentialVerification(ctx context.Context, campaignID string)
 			return call.report, call.err
 		}
 	}
-	call := &verificationCall{done: make(chan struct{})}
+	workCtx, workCancel := context.WithCancel(context.Background())
+	call := &verificationCall{done: make(chan struct{}), workCancel: workCancel}
 	s.verificationCalls[campaignID] = call
 	s.verificationMu.Unlock()
 
-	call.report, call.err = s.loadCredentialVerification(ctx, campaignID)
+	call.register()
+	defer call.leave()
+	go s.runCredentialVerification(call, workCtx, campaignID)
+
+	select {
+	case <-ctx.Done():
+		return audit.FullChainVerification{}, ctx.Err()
+	case <-call.done:
+		return call.report, call.err
+	}
+}
+
+func (s *Service) runCredentialVerification(call *verificationCall, workCtx context.Context, campaignID string) {
+	report, err := s.loadCredentialVerification(workCtx, campaignID)
+	call.report, call.err = report, err
 	s.verificationMu.Lock()
 	delete(s.verificationCalls, campaignID)
-	close(call.done)
 	s.verificationMu.Unlock()
-	return call.report, call.err
+	close(call.done)
 }
 
 func (s *Service) loadCredentialVerification(ctx context.Context, campaignID string) (audit.FullChainVerification, error) {
